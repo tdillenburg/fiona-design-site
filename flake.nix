18dbceb6460ce7326b4e520b4672cd72693f51db
tdillenburg <tdillenburg@users.noreply.github.com>
2023-04-17 04:10:24 -0300

    Using yarn for nodejs in nixos
    Dream2nix failed to build trim-lines, a dependency of mdast-util-to-hast.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,32 @@
 {
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
-  outputs = inp:
-    inp.dream2nix.lib.makeFlakeOutputs {
-      systemsFromFile = ./nix_systems;
-      config.projectRoot = ./.;
-      source = ./.;
-      projects = ./projects.toml;
-    };
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      nodejs_setup = ''
+        # export NODE_PATH=$PWD/.nix/nodejs
+        # export NPM_CONFIG_PREFIX=$PWD/node_modules
+        export PATH=$PWD/node_modules/.bin:$PATH
+        # mkdir -p $NODE_PATH
+      '';
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs 
+        ] ++ (with pkgs.nodePackages; [
+          # pnpm
+          yarn
+          typescript
+          typescript-language-server
+          vscode-langservers-extracted # html, css, js (eslint)
+        ]);
+        shellHook = ''
+          # ${nodejs_setup}
+          # .pnpm-store
+          yarn install
+        '';
+      };
+    });
 }
